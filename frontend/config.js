7aa99ac88eaefc8278dceced9f3da7d4871d7d95
arkvdaogmail <arkvdao@gmail.com>
2025-07-16 23:15:07 -0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // Edit before use:
 window.TS_CONFIG = {
-  STRIPE_PK: "pk_test_REPLACE_WITH_YOURS",
+  STRIPE_PK: "pk_test_51Rl0BCF31XjIKGr08G1IJI7KXsbyCk4MixBol8FbE6EkOpBSlpJZIOrAwE2GLpYBWd2gYiqCIEIjstVmSyxz42xm00QXbjJXOm",
   BACKEND_BASE: "https://REPLACE_BACKEND_BASE" // no trailing slash, no /api
 };
